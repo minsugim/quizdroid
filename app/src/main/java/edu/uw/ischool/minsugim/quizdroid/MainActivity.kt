a7d911caf0aadaf4656c138f5ff9d63daceb9b6c
Minsu Gim <minsugim@uw.edu>
2018-05-15 12:32:56 -0700

    Airplane Mode

--- app/src/main/java/edu/uw/ischool/minsugim/quizdroid/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/minsugim/quizdroid/MainActivity.kt
@@ -1,6 +1,7 @@
 package edu.uw.ischool.minsugim.quizdroid
 
-import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -9,6 +10,17 @@ import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.os.Build
+import android.provider.Settings
+import android.support.v7.app.AlertDialog
+import android.R.string.cancel
+import android.R.string.ok
+import android.content.*
+import android.widget.Toast
+import android.content.Intent
+import android.content.BroadcastReceiver
+
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -23,8 +35,6 @@ class MainActivity : AppCompatActivity() {
             subjects.add(topic.title)
         }
 
-
-
         val listView = findViewById<ListView>(R.id.listView)
         val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, subjects)
         listView.adapter = adapter
@@ -34,6 +44,38 @@ class MainActivity : AppCompatActivity() {
             intent.putExtra("TopicNumber", position)
             startActivity(intent)
         })
+
+        if (isAirplaneModeOn(applicationContext)) {
+            displayAlert()
+        }
+
+        registerReceiver(broadcastReceiver, IntentFilter("AIRPLANE_MODE"))
+    }
+
+    var broadcastReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            displayAlert()
+        }
+    }
+
+    private fun displayAlert() {
+        val builder = AlertDialog.Builder(this)
+        builder.setPositiveButton("Yes", DialogInterface.OnClickListener { dialog, id ->
+            startActivityForResult(Intent(android.provider.Settings.ACTION_AIRPLANE_MODE_SETTINGS), 0)
+        })
+        builder.setNegativeButton("No", DialogInterface.OnClickListener { dialog, id ->
+            dialog.cancel()
+        })
+        builder.setMessage("Airplane mode is on. Would you like to turn off Airplane mode?")
+                .setTitle("Connectivity Issues")
+
+        val dialog = builder.create()
+        dialog.show()
+    }
+
+    private fun isAirplaneModeOn(context: Context): Boolean {
+        return Settings.Global.getInt(context.contentResolver,
+                Settings.Global.AIRPLANE_MODE_ON, 0) != 0
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -50,9 +92,29 @@ class MainActivity : AppCompatActivity() {
         }
 
         else -> {
-            // If we got here, the user's action was not recognized.
-            // Invoke the superclass to handle it.
+
             super.onOptionsItemSelected(item)
+        }
+    }
+
+
+    class NetworkChangeReceiver : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            val cm = context!!.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val activeNetwork: NetworkInfo? = cm.activeNetworkInfo
+            val isConnected: Boolean = activeNetwork?.isConnectedOrConnecting == true
+            if (!isConnected) {
+                if (isAirplaneModeOn(context)) {
+                    context.sendBroadcast(Intent("AIRPLANE_MODE"))
+                }
+            } else {
+                Toast.makeText(context, "Network Error", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        private fun isAirplaneModeOn(context: Context): Boolean {
+            return Settings.Global.getInt(context.contentResolver,
+                    Settings.Global.AIRPLANE_MODE_ON, 0) != 0
         }
     }
 
--- app/src/main/java/edu/uw/ischool/minsugim/quizdroid/Preferences.kt
+++ app/src/main/java/edu/uw/ischool/minsugim/quizdroid/Preferences.kt
@@ -2,8 +2,18 @@ package edu.uw.ischool.minsugim.quizdroid
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Spinner
+import android.view.View
+import android.widget.*
+import android.widget.AdapterView.OnItemSelectedListener
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.AdapterView
+
+
+
+
+
+
 
 class Preferences : AppCompatActivity() {
 
@@ -13,8 +23,14 @@ class Preferences : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preferences)
         val spinner : Spinner = findViewById(R.id.check_interval)
+        val url : EditText = findViewById(R.id.URL)
+        val button : Button = findViewById(R.id.save)
         val adapter: ArrayAdapter<CharSequence> = ArrayAdapter.createFromResource(this, R.array.check_intervals, android.R.layout.simple_spinner_item)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner.adapter = adapter
+
+        button.setOnClickListener {
+            Toast.makeText(this, url.text.toString(), Toast.LENGTH_LONG).show()
+        }
     }
 }
